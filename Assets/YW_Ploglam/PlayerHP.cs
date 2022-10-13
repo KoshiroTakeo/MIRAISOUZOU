6a233fc4f8efe59f186dc20b3a0d0fee35e25d55
KoshiroTakeo <80869009+KoshiroTakeo@users.noreply.github.com>
2022-10-13 18:17:30 +0900

    マージのため、?データコミット

---
+++ Assets/YW_Ploglam/PlayerHP.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class PlayerHP : MonoBehaviour
+{
+    public GameObject life1, life2, life3, life4, life5;
+
+    GameObject player;
+
+    public int nPlayerHP;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nPlayerHP = 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            nPlayerHP--;
+        }
+        if(nPlayerHP==4)
+        {
+            life5.SetActive(false);
+            life4.SetActive(true);
+        }
+        if (nPlayerHP == 3)
+        {
+            life4.SetActive(false);
+            life3.SetActive(true);
+        }
+        if (nPlayerHP == 2)
+        {
+            life3.SetActive(false);
+            life2.SetActive(true);
+        }
+        if (nPlayerHP == 1)
+        {
+            life2.SetActive(false);
+            life1.SetActive(true);
+        }
+    }
+}
