2107a85736fe0f47e7b13cc3de2895b2ed21a097
KoshiroTakeo <80869009+KoshiroTakeo@users.noreply.github.com>
2022-11-17 11:16:27 +0900

    AA

--- Assets/06_Scripts/065_System/GameExit.cs
+++ Assets/06_Scripts/065_System/GameExit.cs
@@ -1,11 +1,10 @@
-<<<<<<< HEAD
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 //using UnityEngine.InputSystem;
 
-public class GameExit : MonoBehaviour,IMusic
+public class GameExit : MonoBehaviour, IMusic
 {
     AudioSource audioSource;
 
@@ -52,61 +51,5 @@ public class GameExit : MonoBehaviour,IMusic
         Application.Quit();//ゲームプレイ終了
                            //#endif
     }
-
-=======
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-//using UnityEngine.InputSystem;
-
-public class GameExit : MonoBehaviour,IMusic
-{
-    AudioSource audioSource;
-
-    public GameObject buttonObj;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        audioSource = GetComponent<AudioSource>();
-    }
-
-    //------------------------------------------------------------------
-    //
-    AudioClip[] IMusic.GetAudioClips()
-    {
-        return MusicPlayer.instance.soundList;
-    }
-
-    void IMusic.PlaySound(int No)
-    {
-        audioSource.PlayOneShot(MusicPlayer.instance.soundList[No]);
-    }
-
-    void IMusic.StopSound()
-    {
-        audioSource.Stop();
-    }
-    //------------------------------------------------------------------
-
-    //ゲーム終了:ボタンから呼び出す
-    public void EndGame()
-    {
-        // SEを流す
-        IMusic iMusic = buttonObj.GetComponent<IMusic>();
-        if (iMusic != null)
-        {
-            iMusic.PlaySound(0);
-        }
-
-        //#if UNITY_EDITOR
-        // エディターから終了させたいとき
-        //UnityEditor.EditorApplication.isPlaying = false;//ゲームプレイ終了
-        //#else
-        Application.Quit();//ゲームプレイ終了
-                           //#endif
-    }
-
->>>>>>> origin/master
 }
+
